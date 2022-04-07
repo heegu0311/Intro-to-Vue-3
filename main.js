1b524778e91af7665c7f08a82b2512d7148f10d0
heegu0311 <heegu0311@gmail.com>
2022-04-07 21:50:29 +0900

    L5 key setting done

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ const app = Vue.createApp({
       inStock: true,
       inventory: 1,
       details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
     };
   },
 });
