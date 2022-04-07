8e94c72e50be6c2f73c325f30c5cd07d2a918846
heegu0311 <heegu0311@gmail.com>
2022-04-08 00:17:25 +0900

    L8 how to use computed properties done

--- main.js
+++ main.js
@@ -3,14 +3,24 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: "Socks",
-      image: "assets/images/socks_green.jpg",
+      brand: "Vue Mastery",
+      selectedVariant: 0,
       url: "https:uni9oo.xyz",
-      inStock: false,
       inventory: 100,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
       ],
     };
   },
@@ -23,8 +33,19 @@ const app = Vue.createApp({
         this.cart -= 1;
       }
     },
-    updateImage(varientImage) {
-      this.image = varientImage;
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
     },
   },
 });
